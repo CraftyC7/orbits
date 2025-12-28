252354083e275c437486642ea54165806a218ffd
CraftyC7 <78454072+CraftyC7@users.noreply.github.com>
2025-12-28 15:15:20 -0500

    Removed descriptions

--- orbits/plugin.cs
+++ orbits/plugin.cs
@@ -12,7 +12,7 @@ namespace orbits;
 [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
 public class plugin : BaseUnityPlugin {
     public const string PluginGUID = "com.fiufki.orbits";
-    public const string PluginName = "Orbits";
+    public const string PluginName = "Orbits Video Only";
     public const string PluginVersion = "1.0.6";
     
     internal static ManualLogSource Logger;
@@ -34,19 +34,19 @@ public class plugin : BaseUnityPlugin {
         Logger.LogInfo($"{PluginName} is loaded.");
     }
     
-    internal static Dictionary<int, (string ClipName, string Description)> LevelPatches = new() { 
-    [0]  = (null, "POPULATION: Abandoned.\nCONDITIONS: Arid. Thick haze, worsened by industrial artifacts.\nFAUNA: Dominated by a few species."),
-    [1]  = (null, "POPULATION: Abandoned.\nCONDITIONS: Jagged and weathered terrain with great foundation.\nFAUNA: Ecosystem supports territorial behaviour."),
-    [2]  = ("MapView56Vow", "POPULATION: Abandoned.\nCONDITIONS: Humid. Rough terrain. Teeming with plant-life.\nFAUNA: A competitive ecosystem supports aggressive lifeforms."),
-    [3]  = ("MapView71Gor", "POPULATION: Unknown.\nCONDITIONS: Continual storms. A complete water mass without any land masses. This is where The Company resides.\nFAUNA: Unknown."),
-    [4]  = ("MapView61Mar", "POPULATION: Abandoned.\nCONDITIONS: Dense holt, expansive terrain. Persistent rain.\nFAUNA: Manifold of danger and valid ecosystem."),
-    [5]  = ("MapView20Ada", "POPULATION: Abandoned.\nCONDITIONS: A landscape of deep valleys and mountains.\nFAUNA: Home to a lively, diverse ecosystem of smaller-sized omnivores."),
-    [6]  = ("MapView85Ren", "POPULATION: None.\nCONDITIONS: Frozen, rocky. Deep mist causes adrift, hard to navigate.\nFAUNA: Orbiting a forlorn ecosystem."),
-    [7]  = ("MapView7Din", "POPULATION: None.\nCONDITIONS: Orbits a white dwarf star. Prior monitoring, for security measures.\nFAUNA: Unlikely for complex life to exist."),
-    [8]  = ("MapView21Off", "POPULATION: Abandoned.\nCONDITIONS: Rocky, bare foundry. With a ragged countryside.\nFAUNA: A ecosystem, gazes for survival."),
-    [9]  = ("MapView8Tit", "POPULATION: None.\nCONDITIONS: Recent constant snow and hot temperatures underground. Thick smog.\nFAUNA: Danger upon every corner."),
-    [10] = ("MapView68Art", "POPULATION: None.\nCONDITIONS: Waning forests. Abandoned facilities littered across the landscape.\nFAUNA: Rumored active machinery left behind."),
-    [12] = ("MapView5Emb", "POPULATION: Abandoned.\nCONDITIONS: Desolate, mostly made out of amethyst and similar crystals.\nFAUNA: Devoided of biological life.")
+    internal static Dictionary<int, string> LevelPatches = new() { 
+    [0]  = (null),
+    [1]  = (null),
+    [2]  = ("MapView56Vow"),
+    [3]  = ("MapView71Gor"),
+    [4]  = ("MapView61Mar"),
+    [5]  = ("MapView20Ada"),
+    [6]  = ("MapView85Ren"),
+    [7]  = ("MapView7Din"),
+    [8]  = ("MapView21Off"),
+    [9]  = ("MapView8Tit"),
+    [10] = ("MapView68Art"),
+    [12] = ("MapView5Emb")
     };
 }
 
@@ -65,13 +65,11 @@ public class RoundManager_Start_Patch {
         foreach (var level in levels)
         {
             if (plugin.LevelPatches.TryGetValue(level.levelID, out var patchData)) {
-                level.LevelDescription = patchData.Description;
-                
-                if (!string.IsNullOrEmpty(patchData.ClipName)) {
-                    if (!_cachedVideoClips.TryGetValue(patchData.ClipName, out var clip)) {
-                        clip = plugin.orbitsAssets.LoadAsset<VideoClip>(patchData.ClipName);
+                if (!string.IsNullOrEmpty(patchData)) {
+                    if (!_cachedVideoClips.TryGetValue(patchData, out var clip)) {
+                        clip = plugin.orbitsAssets.LoadAsset<VideoClip>(patchData);
                         if (clip != null) {
-                            _cachedVideoClips[patchData.ClipName] = clip;
+                            _cachedVideoClips[patchData] = clip;
                         }
                     }
                     
